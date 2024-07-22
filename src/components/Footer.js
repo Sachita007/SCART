32a6082c7d39d09d33f4eb1511139f3726f581ec
Sachita Nand <shachitanandk@gmail.com>
2024-07-22 08:07:06 +0530

    Some Chnages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ function Footer() {
     return (
         <footer>
             <div>
-                <div class="footer-content">
-                    <div class="footer-section">
+                <div className="footer-content">
+                    <div className="footer-section">
                         <h3>About Us</h3>
                         <p>We are a company dedicated to providing high-quality products and services to our customers.</p>
                     </div>
-                    <div class="footer-section">
+                    <div className="footer-section">
                         <h3>Quick Links</h3>
                         <ul>
                             <li><a href="#">Home</a></li>
@@ -19,7 +19,7 @@ function Footer() {
                             <li><a href="#">Contact</a></li>
                         </ul>
                     </div>
-                    <div class="footer-section">
+                    <div className="footer-section">
                         <h3>Contact Us</h3>
                         <ul>
                             <li>Email: shachitanandk@gmail.com</li>
@@ -27,17 +27,17 @@ function Footer() {
                             <li>Address:  Rohini ,Delhi</li>
                         </ul>
                     </div>
-                    <div class="footer-section">
+                    <div className="footer-section">
                         <h3>Follow Us</h3>
-                        <div class="social-icons">
-                            <a href="#" title="Facebook" class="facebook"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" title="Twitter" class="twitter"><i class="fab fa-twitter"></i></a>
-                            <a href="#" title="Instagram" class="instagram"><i class="fab fa-instagram"></i></a>
-                            <a href="#" title="LinkedIn" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+                        <div className="social-icons">
+                            <a href="#" title="Facebook" className="facebook"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" title="Twitter" className="twitter"><i className="fab fa-twitter"></i></a>
+                            <a href="#" title="Instagram" className="instagram"><i className="fab fa-instagram"></i></a>
+                            <a href="#" title="LinkedIn" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="footer-bottom">
+                <div className="footer-bottom">
                     <p>&copy; 2024@ Sachita. All rights reserved.</p>
                 </div>
             </div>
